6d797931a10aab33eeda292826a5f4e6af4fb0bb
Viktor Kirillov <viktorkirillov@MacBook-Pro-Viktor.local>
2018-06-12 14:11:38 +0300

    1

--- examples/test3.py
+++ examples/test3.py
@@ -6,13 +6,12 @@ while True:
     time.sleep(1)
 
 '''
-### BEGIN INIT INFO
-# Provides:          scriptname
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable service provided by daemon.
-### END INIT INFO
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Name=Radio
+Comment=
+Exec=sudo python /home/pi/myprog/radio.py
+Terminal=false
+Type=Application
 '''
