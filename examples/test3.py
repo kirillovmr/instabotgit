a398d651fc263b01645187044d7b40888b02a9ca
Viktor Kirillov <viktorkirillov@MacBook-Pro-Viktor.local>
2018-06-12 13:12:10 +0300

    1

--- examples/test3.py
+++ examples/test3.py
@@ -4,5 +4,15 @@ while True:
     print("TEST 3 | {}".format(i))
     i += 1
     time.sleep(1)
-    if i > 13:
-        break
+
+'''
+### BEGIN INIT INFO
+# Provides:          scriptname
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+'''
