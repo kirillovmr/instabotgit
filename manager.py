8a4ad62cd52e90386b7c2b829989f4c748a8a91c
Viktor Kirillov <viktorkirillov@MacBook-Pro-Viktor.local>
2018-06-14 13:47:10 +0300

    1

--- manager.py
+++ manager.py
@@ -7,13 +7,13 @@ set_actual_zero()
 # time.sleep(100)
 
 while True:
-
+    print(1)
     # Get statuses from database and start/stop bots
     get_bots_status()
-
+    print(2)
     # Check running scripts. If closed - trying to restart
     checkrun()
-
+    print(3)
     # Print running scripts
     print_running_array()
 
