52fbd94836b926ce7f54f413d5d8b83a0e880d85
Viktor Kirillov <viktorkirillov@MacBook-Pro-Viktor.local>
2018-06-14 13:50:19 +0300

    1

--- manager.py
+++ manager.py
@@ -4,16 +4,12 @@ from my_func import *
 # Set all actual values to 0 in database.
 set_actual_zero()
 
-# time.sleep(100)
-
 while True:
-    print(1)
     # Get statuses from database and start/stop bots
     get_bots_status()
-    print(2)
+
     # Check running scripts. If closed - trying to restart
     checkrun()
-    print(3)
     # Print running scripts
     print_running_array()
 
