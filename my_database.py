218637ce73c6389873f6cff028935aa4c60a1ed0
Viktor Kirillov <viktorkirillov@MacBook-Pro-Viktor.local>
2018-06-30 15:40:52 +0300

    1

--- my_database.py
+++ my_database.py
@@ -152,72 +152,72 @@ def get_bots_status():
     # Going through status and start/stop appropriate bots
     settings = db['cursor']
     for data in settings:
-        # print(data)
-        # LIKES
-        if data[like_s_col] != data[like_a_col]:
-            if data[like_s_col] == 1 and data[like_a_col] == 0:
-                start(data[bot_id_col], "like")
-                update_db("like_a", 1, data[bot_id_col])
-            elif data[like_s_col] == 0 and data[like_a_col] == 1:
-                stop(data[bot_id_col], "like")
-                update_db("like_a", 0, data[bot_id_col])
-            elif data[like_s_col] == 9 and data[like_a_col] == 1:
-                restart(data[bot_id_col], "like")
-        # REPOST
-        if data[repo_s_col] != data[repo_a_col]:
-            if data[repo_s_col] == 1 and data[repo_a_col] == 0:
-                start(data[bot_id_col], "repost")
-                update_db("repost_a", 1, data[bot_id_col])
-            elif data[repo_s_col] == 0 and data[repo_a_col] == 1:
-                stop(data[bot_id_col], "repost")
-                update_db("repost_a", 0, data[bot_id_col])
-            elif data[repo_s_col] == 9 and data[repo_a_col] == 1:
-                restart(data[bot_id_col], "repost")
-        # COMMENT
-        if data[comm_s_col] != data[comm_a_col]:
-            if data[comm_s_col] == 1 and data[comm_a_col] == 0:
-                start(data[bot_id_col], "comment")
-                update_db("comment_a", 1, data[bot_id_col])
-            elif data[comm_s_col] == 0 and data[comm_a_col] == 1:
-                stop(data[bot_id_col], "comment")
-                update_db("comment_a", 0, data[bot_id_col])
-            elif data[comm_s_col] == 9 and data[comm_a_col] == 1:
-                restart(data[bot_id_col], "comment")
-        # DIRECT
-        if data[dire_s_col] != data[dire_a_col]:
-            if data[dire_s_col] == 1 and data[dire_a_col] == 0:
-                start(data[bot_id_col], "direct")
-                update_db("direct_a", 1, data[bot_id_col])
-            elif data[dire_s_col] == 0 and data[dire_a_col] == 1:
-                stop(data[bot_id_col], "direct")
-                update_db("direct_a", 0, data[bot_id_col])
-            elif data[dire_s_col] == 9 and data[dire_a_col] == 1:
-                restart(data[bot_id_col], "direct")
-        # FOLLOW
-        if data[foll_s_col] != data[foll_a_col]:
-            if data[foll_s_col] == 1 and data[foll_a_col] == 0:
-                start(data[bot_id_col], "follow")
-                update_db("follow_a", 1, data[bot_id_col])
-            elif data[foll_s_col] == 0 and data[foll_a_col] == 1:
-                stop(data[bot_id_col], "follow")
-                update_db("follow_a", 0, data[bot_id_col])
-            elif data[foll_s_col] == 2 and data[foll_a_col] == 0:
-                start(data[bot_id_col], "unfollow")
-                update_db("follow_a", 2, data[bot_id_col])
-            elif data[foll_s_col] == 0 and data[foll_a_col] == 2:
-                stop(data[bot_id_col], "unfollow")
-                update_db("follow_a", 0, data[bot_id_col])
-            elif data[foll_s_col] == 2 and data[foll_a_col] == 1:
-                stop(data[bot_id_col], "follow")
-                time.sleep(5)
-                start(data[bot_id_col], "unfollow")
-                update_db("follow_a", 2, data[bot_id_col])
-            elif data[foll_s_col] == 1 and data[foll_a_col] == 2:
-                stop(data[bot_id_col], "unfollow")
-                time.sleep(5)
-                start(data[bot_id_col], "follow")
-                update_db("follow_a", 1, data[bot_id_col])
-            elif data[foll_s_col] == 9 and data[foll_a_col] == 1:
-                restart(data[bot_id_col], "follow")
-            elif data[foll_s_col] == 9 and data[foll_a_col] == 2:
-                restart(data[bot_id_col], "unfollow")
+        if True:
+            # LIKES
+            if data[like_s_col] != data[like_a_col]:
+                if data[like_s_col] == 1 and data[like_a_col] == 0:
+                    start(data[bot_id_col], "like")
+                    update_db("like_a", 1, data[bot_id_col])
+                elif data[like_s_col] == 0 and data[like_a_col] == 1:
+                    stop(data[bot_id_col], "like")
+                    update_db("like_a", 0, data[bot_id_col])
+                elif data[like_s_col] == 9 and data[like_a_col] == 1:
+                    restart(data[bot_id_col], "like")
+            # REPOST
+            if data[repo_s_col] != data[repo_a_col]:
+                if data[repo_s_col] == 1 and data[repo_a_col] == 0:
+                    start(data[bot_id_col], "repost")
+                    update_db("repost_a", 1, data[bot_id_col])
+                elif data[repo_s_col] == 0 and data[repo_a_col] == 1:
+                    stop(data[bot_id_col], "repost")
+                    update_db("repost_a", 0, data[bot_id_col])
+                elif data[repo_s_col] == 9 and data[repo_a_col] == 1:
+                    restart(data[bot_id_col], "repost")
+            # COMMENT
+            if data[comm_s_col] != data[comm_a_col]:
+                if data[comm_s_col] == 1 and data[comm_a_col] == 0:
+                    start(data[bot_id_col], "comment")
+                    update_db("comment_a", 1, data[bot_id_col])
+                elif data[comm_s_col] == 0 and data[comm_a_col] == 1:
+                    stop(data[bot_id_col], "comment")
+                    update_db("comment_a", 0, data[bot_id_col])
+                elif data[comm_s_col] == 9 and data[comm_a_col] == 1:
+                    restart(data[bot_id_col], "comment")
+            # DIRECT
+            if data[dire_s_col] != data[dire_a_col]:
+                if data[dire_s_col] == 1 and data[dire_a_col] == 0:
+                    start(data[bot_id_col], "direct")
+                    update_db("direct_a", 1, data[bot_id_col])
+                elif data[dire_s_col] == 0 and data[dire_a_col] == 1:
+                    stop(data[bot_id_col], "direct")
+                    update_db("direct_a", 0, data[bot_id_col])
+                elif data[dire_s_col] == 9 and data[dire_a_col] == 1:
+                    restart(data[bot_id_col], "direct")
+            # FOLLOW
+            if data[foll_s_col] != data[foll_a_col]:
+                if data[foll_s_col] == 1 and data[foll_a_col] == 0:
+                    start(data[bot_id_col], "follow")
+                    update_db("follow_a", 1, data[bot_id_col])
+                elif data[foll_s_col] == 0 and data[foll_a_col] == 1:
+                    stop(data[bot_id_col], "follow")
+                    update_db("follow_a", 0, data[bot_id_col])
+                elif data[foll_s_col] == 2 and data[foll_a_col] == 0:
+                    start(data[bot_id_col], "unfollow")
+                    update_db("follow_a", 2, data[bot_id_col])
+                elif data[foll_s_col] == 0 and data[foll_a_col] == 2:
+                    stop(data[bot_id_col], "unfollow")
+                    update_db("follow_a", 0, data[bot_id_col])
+                elif data[foll_s_col] == 2 and data[foll_a_col] == 1:
+                    stop(data[bot_id_col], "follow")
+                    time.sleep(5)
+                    start(data[bot_id_col], "unfollow")
+                    update_db("follow_a", 2, data[bot_id_col])
+                elif data[foll_s_col] == 1 and data[foll_a_col] == 2:
+                    stop(data[bot_id_col], "unfollow")
+                    time.sleep(5)
+                    start(data[bot_id_col], "follow")
+                    update_db("follow_a", 1, data[bot_id_col])
+                elif data[foll_s_col] == 9 and data[foll_a_col] == 1:
+                    restart(data[bot_id_col], "follow")
+                elif data[foll_s_col] == 9 and data[foll_a_col] == 2:
+                    restart(data[bot_id_col], "unfollow")
